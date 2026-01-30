e6e377fb2191e02d9444e9d7ce1bd2f93a4d9c11
ToniCoding <antoniovallejo2612@gmail.com>
2026-01-30 23:03:34 +0100

    feat: Enforce UTC to avoid problems with timezones

--- src/main/java/com/gadministrativa/GestionAdministrativaApplication.java
+++ src/main/java/com/gadministrativa/GestionAdministrativaApplication.java
@@ -3,9 +3,16 @@ package com.gadministrativa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+/**
+ * The main class for the Spring application.
+ * Only configured thing here is the UTC in order to avoid problems with different time zones.
+ */
 @SpringBootApplication
 public class GestionAdministrativaApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(GestionAdministrativaApplication.class, args);
     }
 }
